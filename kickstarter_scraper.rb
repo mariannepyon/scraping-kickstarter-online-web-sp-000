845be88fbd186f150b9422b368807eda1dbb4479
Marianne Pyon <mariannepyon@gmail.com>
2019-09-13 19:15:22 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,7 +1,13 @@
 # require libraries/modules here
 
 require 'nokogiri'
+require 'pry'
+
+
 
 def create_project_hash
   # write your code here
+  html = File.read('fixtures/kickstarter.html')
+
+  kickstarter = Nokogiri::HTML(html)
 end
